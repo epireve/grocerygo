6d17a945f69c15f75abd9469d7c6b463d4206d56
epireve <i@firdaus.my>
2025-06-08 17:27:53 +0800

    refactor: Phase 1 cleanup - remove temporary files and fix template field references

--- insert_remaining_migrations.sql
+++
@@ -1,6 +0,0 @@
--- Add the remaining migrations to django_migrations table
-INSERT INTO django_migrations (app, name, applied)
-VALUES 
-    ('orders', '0013_fix_migration_graph', datetime('now')),
-    ('orders', '0015_merge_20250608_0024', datetime('now')),
-    ('orders', '0016_fix_orderstatushistory_migration', datetime('now'));
